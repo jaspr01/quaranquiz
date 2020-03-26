24e24f0ba23f76febe6818c5db2d4c714fbec9b3
duckchip <tristan.hofman@telenet.Be>
2020-03-26 21:25:46 +0100

    Added migrations

---
+++ database/migrations/2020_03_26_200616_create_rounds_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoundsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rounds', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->boolean('status');
+            $table->integer('quiz_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('questions', function($table) {
+            $table->foreign('quiz_id')->references('id')->on('quizzes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rounds');
+    }
+}
---
+++ database/migrations/2020_03_26_200620_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('round_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('questions', function($table) {
+            $table->foreign('round_id')->references('id')->on('rounds');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
