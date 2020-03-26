2b289bdcb4ea8bd73a0609e0b99835310373f01c
Jasper Verschuere <verschuere.jasper@gmail.com>
2020-03-26 21:30:09 +0100

    redirect to quizzes index page after register or login

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('quizzes')->middleware('auth')->group(function() {
+    Route::get('/', 'QuizController@index')->name('web.quizzes.index');
+});
